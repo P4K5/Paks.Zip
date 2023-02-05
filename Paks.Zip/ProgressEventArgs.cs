355827cbd2ac8b7ac707701cfe8cf36698f4a565
paks <patrykgatzka@gmail.com>
2023-02-05 14:01:03 +0100

    Dodaj pliki projektów.

---
+++ Paks.Zip/ProgressEventArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace Paks.Zip
+{
+    public class ProgressEventArgs : EventArgs
+    {
+        public int EntriesExtracted { get; private set; }
+        public int EntriesTotal { get; private set; }
+
+        internal ProgressEventArgs(int entriesExtracted, int entriesTotal)
+        {
+            EntriesExtracted = entriesExtracted;
+            EntriesTotal = entriesTotal;
+        }
+    }
+}
---
+++ Paks.Zip/ZipExtractor.cs
@@ -0,0 +1,59 @@
+﻿using System.IO.Compression;
+
+namespace Paks.Zip
+{
+    public class ZipExtractor : IDisposable
+    {
+        private readonly ZipArchive zip;
+        private readonly int entriesTotal;
+        private int _entriesExtracted;
+        
+        public delegate void StatusExtractHandler(object sender, ProgressEventArgs e);
+        public event StatusExtractHandler OnExtractStatus;
+
+        private int EntriesExtracted
+        {
+            get { return _entriesExtracted; }
+            set 
+            {
+                _entriesExtracted = value;
+                if (OnExtractStatus != null)
+                {
+                    ProgressEventArgs args = new ProgressEventArgs(value, entriesTotal);
+                    OnExtractStatus(this, args);
+                }
+            }
+        }
+
+        public ZipExtractor(string zipName)
+        {
+            zip = ZipFile.OpenRead(zipName);
+            entriesTotal = zip.Entries.Count;
+            _entriesExtracted = 0;
+        }
+        
+        public void Extract() => Extract(Directory.GetCurrentDirectory());
+
+        public void Extract(string destinationDirectoryName)
+        {
+            destinationDirectoryName += "/";
+            foreach (ZipArchiveEntry item in zip.Entries)
+            {
+                if (item.FullName[item.FullName.Length - 1] == '/')
+                {
+                    Directory.CreateDirectory(destinationDirectoryName + item.FullName);
+                }
+                else
+                {
+                    item.ExtractToFile(destinationDirectoryName + item.FullName, true);
+                }
+                EntriesExtracted++;
+            }
+        }
+
+        public void Dispose()
+        {
+            zip.Dispose();
+        }
+    }
+}
